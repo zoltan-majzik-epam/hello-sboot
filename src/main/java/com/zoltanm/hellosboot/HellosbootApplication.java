6450ae04f64a27e1f004ac73769608e92d8c9b06
Zoltan Majzik <Zoltan Majzik>
2017-06-07 23:34:04 +0200

    Initial commit for spring boot application

---
+++ src/main/java/com/zoltanm/hellosboot/HellosbootApplication.java
@@ -0,0 +1,12 @@
+package com.zoltanm.hellosboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellosbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellosbootApplication.class, args);
+	}
+}
