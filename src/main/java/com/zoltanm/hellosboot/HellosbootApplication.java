a6372ed2ac93a0eddeeaa4fdf4a145ba3ca514c3
Zoltan Majzik <spiru89@gmail.com>
2017-06-08 22:59:45 +0200

    Addedd actuator, removed mongodb, helloworld endpoint

--- src/main/java/com/zoltanm/hellosboot/HellosbootApplication.java
+++ src/main/java/com/zoltanm/hellosboot/HellosbootApplication.java
@@ -2,11 +2,21 @@ package com.zoltanm.hellosboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HellosbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HellosbootApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
 	}
 }
